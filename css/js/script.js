2e9c1ea46d329cbe3904b4be98960230f2def43b
nozaruddin <nozaruddin222@gmail.com>
2024-08-26 20:42:43 +0700

    tambah gambar

--- css/js/script.js
+++ css/js/script.js
@@ -2,4 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const title = document.querySelector('.typing-title');
     title.classList.add('start-typing');
 });
+// Open the popup
+function openPopup(imgElement) {
+    var popup = document.getElementById("imagePopup");
+    var popupImg = document.getElementById("popupImage");
+
+    popup.style.display = "block";
+    popupImg.src = imgElement.src;
+}
+
+// Close the popup
+function closePopup() {
+    var popup = document.getElementById("imagePopup");
+    popup.style.display = "none";
+}
 
